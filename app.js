9422f8925c86c9daf556fd3abde556855079fd48
Maarten Van den Bergh <Maarten.Van.den.Bergh@gfi.be>
2016-04-26 16:49:35 +0200

    yey

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var redis = require("redis"),
 client = redis.createClient(process.env.REDIS_URL),
 express = require("express"),
-app = express();
+app = express(),
+path = require("path");
+
 
 
 var port = process.env.PORT || 8080;
@@ -10,7 +12,13 @@ client.on("error", function(error){
     console.log("Error" + error);
 });
 
-app.get("/", function(req, res){
+app.use(express.static(__dirname + '/views'));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/views/index.html'));
+});
+
+app.get("/redis", function(req, res){
    client.get("user:username1", function(error, result){
        if(!result){
            var nmscData = "{'nmsc':'aeaeae'}";
