9e6d507f1eedce3666d7cdd8b291263938f2bfdb
Maarten Van den Bergh <Maarten.Van.den.Bergh@gfi.be>
2016-04-29 14:00:46 +0200

    implemented eslint

--- app.js
+++ app.js
@@ -1,53 +1,53 @@
 var redis = require("redis"),
-redis_url = process.env.REDIS_URL || '',
-client = redis.createClient(redis_url),
-port = process.env.PORT || 8080,
-express = require("express"),
-app = express(),
-path = require("path");
+    redis_url = process.env.REDIS_URL || "",
+    client = redis.createClient(redis_url),
+    port = process.env.PORT || 8080,
+    express = require("express"),
+    app = express(),
+    path = require("path");
 
 client.on("error", function(error){
     console.log("Error" + error);
 });
 
-app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname, 'public')));
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/', function(req, res) {
+app.get("/", function(req, res) {
     res.render("index");
 });
 
 app.get("/nmsc", function(req, res){
-   var doneMessage = "responded to get on /nmsc from ";
-   client.get("user:username1", function(error, result){
-       var response = {
-         source : "Redis" 
-       };
+    var doneMessage = "responded to get on /nmsc from ";
+    client.get("user:username1", function(error, result){
+        var response = {
+            source : "Redis" 
+        };
         
-       var nmscData;
-       if(!result){
-           response.source = "TAAS";
-           nmscData = {
-               nmsc:'aeaeae'
-           };
-           client.set("user:username1", JSON.stringify(nmscData));
-       }
-       else{
-           nmscData = JSON.parse(result);
-       }
-       response.data = nmscData;
-       res.send(response);
-       console.log(doneMessage + response.source);
-   });
+        var nmscData;
+        if(!result){
+            response.source = "TAAS";
+            nmscData = {
+                nmsc:"aeaeae"
+            };
+            client.set("user:username1", JSON.stringify(nmscData));
+        }
+        else{
+            nmscData = JSON.parse(result);
+        }
+        response.data = nmscData;
+        res.send(response);
+        console.log(doneMessage + response.source);
+    });
 });
 app.get("/nmsc/delete", function(req, res){
-   var doneMessage = "reponded to get on /nmsc/delete. Nmscdata has been deleted from Redis.";
-   client.del("user:username1", function(error, result){
-      res.send("Nmsc deleted from Redis") ;
-      console.log(doneMessage);
-   });
+    var doneMessage = "reponded to get on /nmsc/delete. Nmscdata has been deleted from Redis.";
+    client.del("user:username1", function(){
+        res.send("Nmsc deleted from Redis") ;
+        console.log(doneMessage);
+    });
 });
 
 app.listen(port, function(){
-    console.log('Our app is running on http://localhost:' + port);
+    console.log("Our app is running on http://localhost:" + port);
 });
